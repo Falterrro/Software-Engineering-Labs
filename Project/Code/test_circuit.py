367ebe1403985b952dad7f7fd38bc456dbd47c57
mmrudapanda <giermala.maciej@gmail.com~>
2026-05-04 22:24:17 +0200

    added circuit class

--- Project/Code/test_circuit.py
+++ Project/Code/test_circuit.py
@@ -1,9 +1,7 @@
-from circuit import Circuit
-
-def test_circuit_add_gate_and_input():
+def test_circuit_evaluates_simple():
     c = Circuit()
     c.add_input("A")
     c.add_input("B")
     c.add_gate("g1", GateType.AND, inputs=["A", "B"])
-    assert "g1" in c.gates
-    asser "g2" in c.inputs
+    result = c.evaluate({"A": True, "B": False})
+    assert result["g1"] == False
