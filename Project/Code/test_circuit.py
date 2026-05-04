9408dcb175fb37f4588f504a6b7464f62801ef7d
Kacper <257201@edu.p.lodz.pl>
2026-05-04 21:56:40 +0200

    Added the first test for correct evaluation of AND gate.

---
+++ Project/Code/test_circuit.py
@@ -0,0 +1,8 @@
+import pytest
+from circuit import Gate, GateType
+
+def test_and_gate_evaluates_correctly():
+    g = Gate(GateType.AND)
+    assert g.evaluate([True, True]) == True
+    assert g.evaluate([True, False]) == False
+    assert g.evaluate([False, False]) == False
